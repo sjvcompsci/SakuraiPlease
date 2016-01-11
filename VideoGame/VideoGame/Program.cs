e833d5761d5fe6e722c08e29c1e77d53548218a5
Mack014 <mscott2016850@sjvhs.com>
2016-01-11 08:34:46 -0500

    adding files

---
+++ VideoGame/VideoGame/Program.cs
@@ -0,0 +1,32 @@
+﻿using Otter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace VideoGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game("Sakurai Plz");
+          
+            var scene = new Scene();
+            var testEntity = new Entity(350, 240);
+           
+            testEntity.AddGraphic(Image.CreateCircle( 100,Color.Red));
+            
+          
+            
+           
+            scene.Add(testEntity);
+
+           
+            game.Start(scene);
+        }
+    }
+
+}
